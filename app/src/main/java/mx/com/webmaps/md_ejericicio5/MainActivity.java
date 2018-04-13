609ad8ed3a718060a5b7a22079307b0a08711291
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-13 14:10:16 -0500

    Se crea un progressBar circular con un ocultamiento a los 8 segundos

--- app/src/main/java/mx/com/webmaps/md_ejericicio5/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejericicio5/MainActivity.java
@@ -1,13 +1,46 @@
 package mx.com.webmaps.md_ejericicio5;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+
+    Handler handler;
+    Runnable runnable;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar= (ProgressBar) findViewById(R.id.progressBar_id);
+        progressBar.setVisibility(View.VISIBLE);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                timer.cancel();
+                progressBar.setVisibility(View.GONE);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        },8000,10000);
+
     }
 }
